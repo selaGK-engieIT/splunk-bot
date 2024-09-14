61e70c376c0da69b846631ab95c09ca23fa45feb
selaGK-engieIT <sela.koshy@engie.com>
2024-09-14 02:20:12 +0200

    Create home.py

---
+++ pages/home.py
@@ -0,0 +1,6 @@
+import streamlit as st
+
+def display():
+    st.title("Splunk Chatbot")
+    st.write("This chatbot is designed to write splunk queries.")
+    st.write("Click on the Chat tab to start a conversation!")
